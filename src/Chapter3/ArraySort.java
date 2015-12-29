df271967a2bb0530014b10eb1f0de360b8583365
Ryo <shiyanch@gmail.com>
2015-12-29 22:20:32 +0800

    add bubble-sort and selection-sort

---
+++ src/Chapter3/ArraySort.java
@@ -0,0 +1,73 @@
+package Chapter3;
+
+public class ArraySort {
+    private long[] a;
+    private int nElems;
+
+    public ArraySort(int max){
+        a = new long[max];
+        nElems = 0;
+    }
+
+    public void insert(long value) {
+        if (nElems == a.length)
+            --nElems;
+        a[nElems++] = value;
+    }
+
+    public void display() {
+        for(int i=0;i<nElems;i++)
+            System.out.print(a[i]+" ");
+        System.out.println(" ");
+    }
+
+    private void swap(int one, int two) {
+        long temp = a[one];
+        a[one] = a[two];
+        a[two] = temp;
+    }
+
+    public void bubleSort() {
+        int in,out;
+
+        for (out=nElems-1;out>0;out--) {
+            for (in=0;in<out;in++) {
+                if (a[in] > a[in+1])
+                    swap(in,in+1);
+            }
+        }
+    }
+
+    public void selectionSort() {
+        int out,in,min;
+
+        for (out=0;out<nElems-1;out++) {
+            min = out;
+            for(in=out+1;in<nElems;in++) {
+                if(a[in] < a[min])
+                    min = in;
+            }
+            swap(out, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        int maxSize = 10;
+        ArraySort arr = new ArraySort(maxSize);
+
+        arr.insert(7);
+        arr.insert(17);
+        arr.insert(34);
+        arr.insert(56);
+        arr.insert(23);
+        arr.insert(56);
+        arr.insert(12);
+
+        arr.display();
+
+        arr.selectionSort();
+        arr.display();
+
+    }
+
+}

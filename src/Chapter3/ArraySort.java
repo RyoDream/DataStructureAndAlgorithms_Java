451e3e823cb77f6dbf3862f04abb12c4cd8fcce6
Ryo <shiyanch@gmail.com>
2016-01-02 20:32:57 +0800

    complete some homework

--- src/Chapter3/ArraySort.java
+++ src/Chapter3/ArraySort.java
@@ -1,5 +1,7 @@
 package Chapter3;
 
+import javax.sound.midi.SysexMessage;
+
 public class ArraySort {
     private long[] a;
     private int nElems;
@@ -25,6 +27,7 @@ public class ArraySort {
         long temp = a[one];
         a[one] = a[two];
         a[two] = temp;
+        display();
     }
 
     public void bubbleSort() {
@@ -35,6 +38,28 @@ public class ArraySort {
                 if (a[in] > a[in+1])
                     swap(in,in+1);
             }
+        }
+    }
+
+    // 双向bubblesort
+    public void shakedBubbleSort() {
+        int shift,left=0,right=nElems-1;
+
+        while(right>left) {
+            shift=left;
+            while (shift<right) {
+                if(a[shift] > a[shift+1])
+                    swap(shift,shift+1);
+                shift++;
+            }
+
+            while(shift > left) {
+                if (a[shift] < a[shift-1])
+                    swap(shift,shift-1);
+                shift--;
+            }
+            left++;
+            right--;
         }
     }
 
@@ -57,12 +82,39 @@ public class ArraySort {
         for(out=1;out<nElems;out++) {
             long temp = a[out];
             in = out;
+
+            // a[in-1] >= temp :不稳定
+            // a[in-1] > temp :稳定
             while (in > 0 && a[in-1]>=temp) {
                 a[in] = a[in-1];
                 --in;
+
             }
             a[in] = temp;
         }
+    }
+
+    public void noDups() {
+        insertionSort();
+        long[] result = new long[nElems];
+
+        int j=0;
+        for(int i=0;i<nElems;i++) {
+            if(a[i] != a[i+1]) {
+                result[j++] = a[i];
+            }
+        }
+        a = result;
+        nElems = j;
+    }
+
+    public double median() {
+        insertionSort();
+
+        if(nElems%2 == 0)
+            return (double)(a[nElems/2]+a[nElems/2-1])/2.0;
+        else
+            return (double)a[nElems/2];
     }
 
     public static void main(String[] args) {
@@ -76,12 +128,17 @@ public class ArraySort {
         arr.insert(23);
         arr.insert(56);
         arr.insert(12);
+        arr.insert(13);
 
         arr.display();
 
 //        arr.bubbleSort();
 //        arr.selectionSort();
-        arr.insertionSort();
+//        arr.insertionSort();
+//        arr.shakedBubbleSort();
+//        arr.display();
+
+        arr.noDups();
         arr.display();
 
     }

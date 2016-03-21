486cf8b59458c5af0397b90beeb595fd8d790ae5
Ryo <shiyanch@gmail.com>
2016-03-22 07:52:24 +0800

    Implementation of Graph and DFS.

---
+++ src/Chapter13/GraphDFS.java
@@ -0,0 +1,83 @@
+package Chapter13;
+
+public class GraphDFS {
+    public final int MAX_VERTS = 20;
+    private Vertex[] vertexList;
+    private int[][] adjacencyMatrix;
+    private int nVertexs;
+    private StackX theStack;
+
+    public GraphDFS() {
+        vertexList = new Vertex[MAX_VERTS];
+
+        adjacencyMatrix = new int[MAX_VERTS][MAX_VERTS];
+        nVertexs = 0;
+
+        for (int i = 0; i < MAX_VERTS; i++)
+            for (int j = 0; j < MAX_VERTS; j++)
+                adjacencyMatrix[i][j] = 0;
+
+        theStack = new StackX();
+    }
+
+    public void addVertex(char lab) {
+        vertexList[nVertexs++] = new Vertex(lab);
+    }
+
+    public void addEdge(int start, int end) {
+        adjacencyMatrix[start][end] = 1;
+        adjacencyMatrix[end][start] = 1;
+    }
+
+    public void displayVertex(int v) {
+        System.out.print(vertexList[v].label);
+    }
+
+    public void dfs() {
+        vertexList[0].wasVisited = true;
+        displayVertex(0);
+        theStack.push(0);
+
+        while (!theStack.isEmpty()) {
+            int v = getAdjUnvisitedVertex(theStack.peek());
+            if (v == -1)
+                theStack.pop();
+            else {
+                vertexList[v].wasVisited = true;
+                displayVertex(v);
+                theStack.push(v);
+            }
+        }
+
+        for (int i = 0; i < nVertexs; i++)
+            vertexList[i].wasVisited = false;
+    }
+
+    public int getAdjUnvisitedVertex(int v) {
+        for (int i = 0; i < nVertexs; i++)
+            if (adjacencyMatrix[v][i] == 1 && !vertexList[i].wasVisited)
+                return i;
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        GraphDFS theGraph = new GraphDFS();
+
+        theGraph.addVertex('A');
+        theGraph.addVertex('B');
+        theGraph.addVertex('C');
+        theGraph.addVertex('D');
+        theGraph.addVertex('E');
+
+        theGraph.addEdge(0, 1);
+        theGraph.addEdge(1, 2);
+        theGraph.addEdge(0, 3);
+        theGraph.addEdge(3, 4);
+
+        System.out.print("Visites: ");
+        theGraph.dfs();
+        System.out.println();
+
+    }
+}
---
+++ src/Chapter13/StackX.java
@@ -0,0 +1,28 @@
+package Chapter13;
+
+public class StackX {
+    private final int SIZE = 20;
+    private int[] st;
+    private int top;
+
+    public StackX() {
+        st = new int[SIZE];
+        top = -1;
+    }
+
+    public void push(int value) {
+        st[++top] = value;
+    }
+
+    public int pop() {
+        return st[top--];
+    }
+
+    public int peek() {
+        return st[top];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+}

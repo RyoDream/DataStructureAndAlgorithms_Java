41cf7ab64b8ea0de656e0be14b6680d6bc6f2b66
Ryo <shiyanch@gmail.com>
2016-05-18 10:39:47 +0800

    implementation of Separate Chaining Hash Table

--- src/New_Chapter5/SeparateChainingHashTable.java
+++ src/New_Chapter5/SeparateChainingHashTable.java
@@ -86,12 +86,40 @@ public class SeparateChainingHashTable<AnyType> {
         return hashVal;
     }
 
+    /**
+     * Internal method to find a prime number no less than n.
+     *
+     * @param n the starting number.
+     * @return a prime number larger than or equal to n.
+     */
     private static int nextPrime(int n) {
-        return 0;
+        if (n % 2 == 0)
+            n++;
+
+        while (!isPrime(n))
+            n += 2;
+
+        return n;
     }
 
+    /**
+     * Internal method to test if a number is prime.
+     * @param n the number to test.
+     * @return the result of the test.
+     */
     private static boolean isPrime(int n) {
-        return false;
+
+        if (n == 2 || n == 3)
+            return true;
+
+        if (n == 1 || n % 2 == 0)
+            return false;
+
+        for (int i = 3; i * i <= n; i += 2)
+            if (n % i == 0)
+                return false;
+
+        return true;
     }
 
 
